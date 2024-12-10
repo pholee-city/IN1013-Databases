b47876376cb7faf66f42a67e911cdb18d8ba34b8
Phoebe L <Phoebe.Lee@city.ac.uk>
2024-12-10 09:44:36 +0000

    Update task.sql

--- HW 7/task.sql
+++ HW 7/task.sql
@@ -1,15 +1,33 @@
 --1
 
+CREATE VIEW samsBills AS
+SELECT s.first_name, s.surname, b.bill_date, b.cust_name, b.bill_total
+FROM restBill b
+JOIN restStaff s ON b.waiter_no = s.staff_no
+WHERE s.first_name = 'Sam' AND s.surname = 'Pitt';
 
 
 --2
 
+SELECT *
+FROM samsBills
+WHERE bill_total > 400;
 
 
 --3
 
+CREATE VIEW roomTotals AS
+SELECT rt.room_name, SUM(b.bill_total) AS total_sum
+FROM restBill b
+JOIN restRest_table rt ON b.table_no = rt.table_no
+GROUP BY rt.room_name;
 
 
 --4
 
-
+CREATE VIEW teamTotals AS
+SELECT CONCAT(h.first_name, ' ', h.surname) AS headwaiter_name, SUM(b.bill_total) AS total_sum
+FROM restStaff h
+JOIN restStaff w ON h.staff_no = w.headwaiter
+JOIN restBill b ON w.staff_no = b.waiter_no
+GROUP BY h.first_name, h.surname;
